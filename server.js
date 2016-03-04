330df6bd0053d86800e0a1d0c0b3a97446fe16b9
Maikel <jmmerillas@ipartek.com>
2016-03-04 12:42:21 +0100

    Mongo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -7,6 +8,14 @@ app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.send('Hello world from github');
+});
+
+app.get('/db' , function (req, res) {
+  mongoose.connect('mongodb://test:test@ds039321.mlab.com:39321/prueba', function(err, res) {
+    if(err) {
+      console.log('ERROR: connecting to Database. ' + err);
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
