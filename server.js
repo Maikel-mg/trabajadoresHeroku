38bc49657b3600e35c74d906874de65965a2ffab
Maikel <jmmerillas@ipartek.com>
2016-03-07 12:09:18 +0100

    DB mongo

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+
+
+var db;
 var trabajadorSchema = new mongoose.Schema({
   id: String,
   nombre: String
@@ -8,10 +11,28 @@ var trabajadorSchema = new mongoose.Schema({
 var Trabajador = mongoose.model('trabajadores', trabajadorSchema);
 var schema = mongoose.Schema({ name: 'string' });
 
-var nuevo;
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/public', express.static(__dirname + '/public'));
+
+
+
+try {
+  console.log('MONGOLAB_URI :: ' + process.env.MONGOLAB_URI);
+  db = mongoose.createConnection(process.env.MONGOLAB_URI);
+
+  console.log('OK : connecting to Database. ');
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+} catch (e) {
+  console.log('ERROR: connecting to Database. ' + e);
+} finally {
+  console.log('FINALLY: connecting to Database. ' );
+}
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello world from github');
@@ -51,6 +72,34 @@ app.get('/dbmongo' , function (req, res) {
   // });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/cats' , function (req, res) {
+
+  try {
+    var Cat = db.model('Cat', schema);
+
+
+    var query = Cat.find({}, null, { skip: 10 });
+    var promise = query.exec();
+    promise.addBack(function (err, docs) {
+      if (err) {
+      console.log('REEORmeow' + error);
+      }
+      res.json(docs);
+    });
+
+  } catch (e) {
+    console.log('ERROR: connecting to Database. ' + e);
+  } finally {
+    console.log('FINALLY: connecting to Database. ' );
+  }
+
+  // mongoose.connect('mongodb://test:test@ds039321.mlab.com:39321/prueba', function(err, res) {
+  //   if(err) {
+  //     console.log('ERROR: connecting to Database. ' + err);
+  //   }
+  // });
 });
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
