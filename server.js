9f7912f1b5fc2c5cd49ff05d7bacd03895494b27
Maikel <jmmerillas@ipartek.com>
2016-03-07 12:30:59 +0100

    DB mongo

--- server.js
+++ server.js
@@ -32,7 +32,11 @@ try {
   console.log('FINALLY: connecting to Database. ' );
 }
 
-
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 app.get('/', function (req, res) {
   res.send('Hello world from github');
