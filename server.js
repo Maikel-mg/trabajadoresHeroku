333c40524d95e27c07c634d488c1521e7175de10
Maikel <jmmerillas@ipartek.com>
2016-03-07 10:25:04 +0100

    DB pg

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,11 +11,38 @@ app.get('/', function (req, res) {
   res.send('Hello world from github');
 });
 
-app.get('/db' , function (req, res) {
+app.get('/dbmongo' , function (req, res) {
   var db;
   try {
     db = mongoose.createConnection('mongodb://user:pass@localhost:port/database');
     console.log('OK : connecting to Database. ');
+  } catch (e) {
+    console.log('ERROR: connecting to Database. ' + e);
+  } finally {
+  node   console.log('FINALLY: connecting to Database. ' );
+  }
+
+  // mongoose.connect('mongodb://test:test@ds039321.mlab.com:39321/prueba', function(err, res) {
+  //   if(err) {
+  //     console.log('ERROR: connecting to Database. ' + err);
+  //   }
+  // });
+});
+
+app.get('/dbpg' , function (req, res) {
+  var db;
+  try {
+    pg.defaults.ssl = true;
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+
+    client
+      .query('SELECT table_schema,table_name FROM information_schema.tables;')
+      .on('row', function(row) {
+        console.log(JSON.stringify(row));
+      });
+  });
   } catch (e) {
     console.log('ERROR: connecting to Database. ' + e);
   } finally {
@@ -27,6 +55,8 @@ app.get('/db' , function (req, res) {
   //   }
   // });
 });
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
